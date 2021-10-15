402e19319a3ed94c9cb1064a530b881d07913624
Cool_Tkinter_Projects <crystaled2003@gmail.com>
2021-10-15 18:09:30 +0530

    Food Delivery

---
+++ javascript/functions.js
@@ -0,0 +1,55 @@
+document.getElementById('submitButton').addEventListener('mouseover',()=>{
+    document.getElementById('submitButton').style.width='35vw';
+    document.getElementById('submitButton').style.transition='all 0.5s ease-in-out';
+});
+
+document.getElementById('submitButton').addEventListener('mouseleave',()=>{
+    document.getElementById('submitButton').style.width='20vw';
+    document.getElementById('submitButton').style.transition='all 0.5s ease-in-out';
+});
+
+document.getElementById('nameEntry').addEventListener('mouseover',()=>{
+    document.getElementById('nameEntry').style.width='40vw';
+    document.getElementById('nameEntry').style.borderRadius='50px';
+    document.getElementById('nameEntry').style.transition='all 0.5s ease-in-out';
+});
+
+document.getElementById('ageEntry').addEventListener('mouseover',()=>{
+    document.getElementById('ageEntry').style.width='40vw';
+    document.getElementById('ageEntry').style.borderRadius='50px';
+    document.getElementById('ageEntry').style.transition='all 0.5s ease-in-out';
+});
+
+document.getElementById('emailEntry').addEventListener('mouseover',()=>{
+    document.getElementById('emailEntry').style.width='40vw';
+    document.getElementById('emailEntry').style.borderRadius='50px';
+    document.getElementById('emailEntry').style.transition='all 0.5s ease-in-out';
+});
+
+document.getElementById('feebackEntry').addEventListener('mouseover',()=>{
+    document.getElementById('feebackEntry').style.width='40vw';
+    document.getElementById('feebackEntry').style.transition='all 0.5s ease-in-out';
+});
+
+document.getElementById('nameEntry').addEventListener('mouseleave',()=>{
+    document.getElementById('nameEntry').style.width='35vw';
+    document.getElementById('nameEntry').style.borderRadius='5px';
+    document.getElementById('nameEntry').style.transition='all 0.5s ease-in-out';
+});
+
+document.getElementById('ageEntry').addEventListener('mouseleave',()=>{
+    document.getElementById('ageEntry').style.width='35vw';
+    document.getElementById('ageEntry').style.borderRadius='5px';
+    document.getElementById('ageEntry').style.transition='all 0.5s ease-in-out';
+});
+
+document.getElementById('emailEntry').addEventListener('mouseleave',()=>{
+    document.getElementById('emailEntry').style.width='35vw';
+    document.getElementById('emailEntry').style.borderRadius='5px';
+    document.getElementById('emailEntry').style.transition='all 0.5s ease-in-out';
+});
+
+document.getElementById('feebackEntry').addEventListener('mouseleave',()=>{
+    document.getElementById('feebackEntry').style.width='35vw';
+    document.getElementById('feebackEntry').style.transition='all 0.5s ease-in-out';
+});
