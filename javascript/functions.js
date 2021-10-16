df4a6422f7ed16f8bf8ffa4820a4df2906110c37
Cool_Tkinter_Projects <crystaled2003@gmail.com>
2021-10-16 16:27:15 +0530

    Food Delivery

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,56 +1,56 @@
 document.getElementById('submitButton').addEventListener('mouseover',()=>{
-    document.getElementById('submitButton').style.width='35vw';
+    document.getElementById('submitButton').style.width='80%';
     document.getElementById('submitButton').style.transition='all 0.5s ease-in-out';
 });
 
 document.getElementById('submitButton').addEventListener('mouseleave',()=>{
-    document.getElementById('submitButton').style.width='20vw';
+    document.getElementById('submitButton').style.width='50%';
     document.getElementById('submitButton').style.transition='all 0.5s ease-in-out';
 });
 
 document.getElementById('nameEntry').addEventListener('mouseover',()=>{
-    document.getElementById('nameEntry').style.width='40vw';
+    document.getElementById('nameEntry').style.width='85%';
     document.getElementById('nameEntry').style.borderRadius='50px';
     document.getElementById('nameEntry').style.transition='all 0.5s ease-in-out';
 });
 
 document.getElementById('ageEntry').addEventListener('mouseover',()=>{
-    document.getElementById('ageEntry').style.width='40vw';
+    document.getElementById('ageEntry').style.width='85%';
     document.getElementById('ageEntry').style.borderRadius='50px';
     document.getElementById('ageEntry').style.transition='all 0.5s ease-in-out';
 });
 
 document.getElementById('emailEntry').addEventListener('mouseover',()=>{
-    document.getElementById('emailEntry').style.width='40vw';
+    document.getElementById('emailEntry').style.width='85%';
     document.getElementById('emailEntry').style.borderRadius='50px';
     document.getElementById('emailEntry').style.transition='all 0.5s ease-in-out';
 });
 
 document.getElementById('feebackEntry').addEventListener('mouseover',()=>{
-    document.getElementById('feebackEntry').style.width='40vw';
+    document.getElementById('feebackEntry').style.width='85%';
     document.getElementById('feebackEntry').style.transition='all 0.5s ease-in-out';
 });
 
 document.getElementById('nameEntry').addEventListener('mouseleave',()=>{
-    document.getElementById('nameEntry').style.width='35vw';
+    document.getElementById('nameEntry').style.width='80%';
     document.getElementById('nameEntry').style.borderRadius='5px';
     document.getElementById('nameEntry').style.transition='all 0.5s ease-in-out';
 });
 
 document.getElementById('ageEntry').addEventListener('mouseleave',()=>{
-    document.getElementById('ageEntry').style.width='35vw';
+    document.getElementById('ageEntry').style.width='80%';
     document.getElementById('ageEntry').style.borderRadius='5px';
     document.getElementById('ageEntry').style.transition='all 0.5s ease-in-out';
 });
 
 document.getElementById('emailEntry').addEventListener('mouseleave',()=>{
-    document.getElementById('emailEntry').style.width='35vw';
+    document.getElementById('emailEntry').style.width='80%';
     document.getElementById('emailEntry').style.borderRadius='5px';
     document.getElementById('emailEntry').style.transition='all 0.5s ease-in-out';
 });
 
 document.getElementById('feebackEntry').addEventListener('mouseleave',()=>{
-    document.getElementById('feebackEntry').style.width='35vw';
+    document.getElementById('feebackEntry').style.width='80%';
     document.getElementById('feebackEntry').style.transition='all 0.5s ease-in-out';
 });
 
@@ -59,11 +59,11 @@ var containerHeight='17vh';
 function showhidemenu(){
     
     if(containerHeight=='17vh'){
-        document.getElementById('container').style.height='38vh';
+        document.getElementById('container').style.height='40vh';
         document.getElementById('container').style.transition='height 0.5s ease-in-out';
-        containerHeight='38vh';
+        containerHeight='40vh';
     } 
-    else if(containerHeight=='38vh'){
+    else if(containerHeight=='40vh'){
         document.getElementById('container').style.height='17vh';
         document.getElementById('container').style.transition='height 0.5s ease-in-out';
         containerHeight='17vh';
